ed7dc1353465719cce2ce3865f7c36d65cc418af
david <david@daviddeMacBook-Pro.local>
2024-01-30 21:26:38 +0700

    update

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ services='gateway user domain email'
 
 for service in $services
 do
-  docker build -f gateway/Dockerfile -t $harborurl/zero/$service:latest .
+  docker build -f $service/Dockerfile -t $harborurl/zero/$service:latest .
   docker push $harborurl/zero/$service:latest
 done
 
